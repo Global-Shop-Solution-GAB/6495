b5b58b38edeae7db571dc83eadc0ea0d56ad1e64
Service Web <sw@gssmail.com>
2024-03-18 16:05:39 -0500

    Commit changes in C:\Websites\Git\emarlow\6495\GCG_6495_HIST_DETAIL.sql

---
+++ GCG_6495_HIST_DETAIL.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "GCG_6495_HIST_DETAIL" (
+ "RequestID" INTEGER NOT NULL,
+ "Approved" BIT NOT NULL,
+ "Rejected" BIT NOT NULL,
+ "FieldName" CHAR(30),
+ "PrevVal" CHAR(50),
+ "ReqVal" CHAR(50) );
