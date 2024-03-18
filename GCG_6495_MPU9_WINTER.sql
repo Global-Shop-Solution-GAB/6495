5d6d9ce6644a275004acc43f9bbbbfd46642bff4
Service Web <sw@gssmail.com>
2024-03-18 16:05:39 -0500

    Commit changes in C:\Websites\Git\emarlow\6495\GCG_6495_MPU9_WINTER.sql

---
+++ GCG_6495_MPU9_WINTER.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "GCG_6495_MPU9_WINTER" (
+ "UpdateDate" DATE,
+ "Grade" CHAR(4),
+ "Size" CHAR(16),
+ "ActivatorPerToAdd" NUMERIC(5,2),
+ "ActivatorType" CHAR(30),
+ "CatalystPerToAdd" NUMERIC(5,2),
+ "PourTemp" NUMERIC(5,0),
+ "PourWeight" NUMERIC(5,0),
+ "DyeType" CHAR(30),
+ "DyePerToAdd" NUMERIC(5,2),
+ "AdditionalType" CHAR(30),
+ "AdditionalPerToAdd" NUMERIC(5,2),
+ "AdditionalAdditivesType" CHAR(30),
+ "AdditionalAdditivesPerToAdd" NUMERIC(5,2),
+ "BalanceCaproPerToAdd" NUMERIC(5,2));
